e1e6aa2e5997e1f4ab08009c41c96a624447f58b
Christ Pentecostal Praise Family <89534730+NiiOdartei1@users.noreply.github.com>
2024-11-03 07:26:41 +0000

    Delete games directory

--- games/js/startup.js
+++
@@ -1,5 +0,0 @@
-setTimeout(() => {
-    document.getElementById("startup-screen").classList.add("hidden");
-    document.getElementById("game-container").classList.remove("hidden"); // Show game elements
-  }, 1000); // Change to 1 second for testing
-  
